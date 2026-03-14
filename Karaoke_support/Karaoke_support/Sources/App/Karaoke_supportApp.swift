c0d67c96f092db42b38083f0d40ed9a6973bbfde
Seiya Suzuki <ikpubon@icloud.com>
2026-03-14 21:05:03 +0900

    efactor: AppエントリとRootViewを分離

---
+++ Karaoke_support/Karaoke_support/Sources/App/Karaoke_supportApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Karaoke_supportApp: App {
+	var body: some Scene {
+		WindowGroup {
+			RootView()
+		}
+	}
+}
---
+++ Karaoke_support/Karaoke_support/Sources/Presentation/Root/RootView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct RootView: View {
+	var body: some View {
+		Text("Karaoke support")
+			.padding()
+	}
+}
+
+#Preview {
+	RootView()
+}
