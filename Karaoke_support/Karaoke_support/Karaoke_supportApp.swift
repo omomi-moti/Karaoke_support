f30504068f1f3660ef66f623d35275687e88bee2
Seiya Suzuki <ikpubon@icloud.com>
2026-03-14 18:47:22 +0900

    feat: アプリ起動ビューをRootViewへ切り替え

---
+++ Karaoke_support/Karaoke_support/Karaoke_supportApp.swift
@@ -0,0 +1,24 @@
+//
+//  Karaoke_supportApp.swift
+//  Karaoke_support
+//
+//  Created by 鈴木聖也 on 2026/03/14.
+//
+
+import SwiftUI
+
+@main
+struct Karaoke_supportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
+
+private struct RootView: View {
+    var body: some View {
+        Text("Karaoke support")
+            .padding()
+    }
+}
