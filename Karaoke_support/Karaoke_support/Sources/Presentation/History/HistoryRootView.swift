f238dca03334db3451e13ee8cb09c304ec545185
Seiya Suzuki <ikpubon@icloud.com>
2026-03-22 16:54:21 +0900

    refactor: HistoryRootView を HistoryListContainerView 経由に変更

--- Karaoke_support/Karaoke_support/Sources/Presentation/History/HistoryRootView.swift
+++ Karaoke_support/Karaoke_support/Sources/Presentation/History/HistoryRootView.swift
@@ -2,21 +2,9 @@ import SwiftUI
 
 struct HistoryRootView: View {
 	@Environment(\.sessionRepository) private var sessionRepository
-	@State private var viewModel: HistoryViewModel?
 
 	var body: some View {
-		Group {
-			if let vm = viewModel {
-				HistoryListView(viewModel: vm)
-			} else {
-				Color.clear
-					.frame(maxWidth: .infinity, maxHeight: .infinity)
-					.onAppear {
-						guard viewModel == nil else { return }
-						viewModel = HistoryViewModel(sessionRepository: sessionRepository)
-					}
-			}
-		}
+		HistoryListContainerView(sessionRepository: sessionRepository)
 	}
 }
 
